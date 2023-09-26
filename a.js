b6d33a87ea96998659718485055689665dbed68f
DANTE SIEGEL <47492509@ort>
2023-09-26 08:58:13 -0300

    aaaa

--- a.js
+++ a.js
@@ -12,8 +12,15 @@ function obtenerEdad(fechaNacimiento) {
 
 const Frutas = ['Manzana','Banana','Naranja','Frutilla','Uva','Piña','Mango','Sandia','Kiwi','Melocoton']
 
+const ciudad = {
+nombre: "ciudad Amor",
+fechaFundacion: 09/9/1999,
+poblacion:2999999,
+extension: 400 + "KM"
 
+};
 
+const dobleElementos = [1,2,3,4]
 
 
 
@@ -31,10 +38,19 @@ else{
     console.log("No tenemos " + frut);
 } 
 
+ console.log(10 == '10') // true 
+ console.log(10 ==='10')// false x q el tipo de dato es distinto y el === compara el tipo de dato tambien
+ console.log(10.6)//es un const o un float
+ console.log(true == 1)//true es considerado == 1 
 
 
+for (const property in ciudad) {
+    console.log(`${property} : ${ciudad[property]}`);
+  }
 
 
-
-
-
+  for (const elemento of dobleElementos) {
+    console.log(elemento);
+    console.log(elemento);
+  }
+  
